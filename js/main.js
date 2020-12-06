f032bf019cdf2c496c5d4a56d84b8d00085f1706
hosseinali9621169203hosseini <72683612+hosseinali9621169203hosseini@users.noreply.github.com>
2020-12-06 10:15:16 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 //java-script
+console.log(document.getElementById('my-header'));
 let headerTitle = document.getElementById('my-header');
-
-headerTitle.innerHTML = '<h3>سلام</h3>';
-headerTitle.style.borderBottom = 'solid 3px #000';
+let title = document.getElementById('title-content');
+console.log(headerTitle);
+headerTitle.textContent = 'حسینعلی حسینی';
+title.innerText = 'تمرین 2';
+console.log(headerTitle.innerText);
+title.style.borderBottom = 'solid 3px #000';
