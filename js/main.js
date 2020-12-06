ab4c6a3b2712ed58bc3de232eb904361f9a8aa06
hosseinali9621169203hosseini <72683612+hosseinali9621169203hosseini@users.noreply.github.com>
2020-12-06 09:57:54 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //java-script
 let headerTitle = document.getElementById('my-header');
 
-headerTitle.innerHTML = '<h3>Hello</h3>';
+headerTitle.innerHTML = '<h3>سلام</h3>';
 headerTitle.style.borderBottom = 'solid 3px #000';
