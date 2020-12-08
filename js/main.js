bc9eb5de59621e1d08a8d215dc8a34794137c13d
hosseinali9621169203hosseini <72683612+hosseinali9621169203hosseini@users.noreply.github.com>
2020-12-08 20:24:13 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
 //java-script
-var items = document.getElementsByClassName('title-content');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-
-// Gives error
-//items.style.backgroundColor = '#f4f4f4';
-
-for(var i = 0; i < items.length; i++){
-  items[i].style.backgroundColor = '#f4f4f4';
-}
