e8a47e66e82062eb2ecf38ebdbdd9c69ff6c601f
hosseinali9621169203hosseini <72683612+hosseinali9621169203hosseini@users.noreply.github.com>
2021-01-30 16:18:38 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 //java-script
+
+let x = document.getElementsByClassName('title');
+x[0].innerHTML = "درباره جهانگرد";
+x[1].innerHTML = "حرکت کن...";
+
+document.getElementById('quotation').innerText = '" دنیا مانند یک کتاب است و کسانی که به سفر نرفته اند، تنها یک صفحه آن را خوانده اند. "';
+document.getElementById('teller').innerText = '(سنت آگوستین)';
