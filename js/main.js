228c95332c1d7b5ff60fa78f5f9e043e696b1c5b
hosseinali9621169203hosseini <72683612+hosseinali9621169203hosseini@users.noreply.github.com>
2021-01-30 17:09:42 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-//java-script
 
 let x = document.getElementsByClassName('title');
 x[0].innerHTML = "درباره جهانگرد";
 x[1].innerHTML = "حرکت کن...";
 
+let y = document.getElementsByClassName('txtgrid');
+y[0].innerText = 'در مورد سازمان، ماموریت، روش های ما و نتایج دهها سال سیاحت، شادی و آرامش را ببینید.';
+y[1].innerText = 'آماده گام بعدی است؟ شما می توانید به عنوان یک مشارکت کننده به ما کمک کنید یا شرکت کنید.';
+
+
 document.getElementById('quotation').innerText = '" دنیا مانند یک کتاب است و کسانی که به سفر نرفته اند، تنها یک صفحه آن را خوانده اند. "';
 document.getElementById('teller').innerText = '(سنت آگوستین)';
+
+
