328ae1d8649fe16418f28c7bba0b406bdd78891e
hosseinali9621169203hosseini <72683612+hosseinali9621169203hosseini@users.noreply.github.com>
2021-01-30 17:43:22 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let x = document.getElementsByClassName('title');
 x[0].innerHTML = "درباره جهانگرد";
 x[1].innerHTML = "حرکت کن...";
 
-let y = document.getElementsByClassName('txtgrid');
+let y = document.getElementsByClassName('txt-grid');
 y[0].innerHTML = 'در مورد سازمان، ماموریت، روش های ما و نتایج دهها سال سیاحت، شادی و آرامش را ببینید.';
 y[1].innerHTML = 'آماده گام بعدی است؟ شما می توانید به عنوان یک مشارکت کننده به ما کمک کنید یا شرکت کنید.';
 
